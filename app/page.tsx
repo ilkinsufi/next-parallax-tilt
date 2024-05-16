680e083390e7b229a092cad4c8f16bc30521fefa
ilkinsufi <ilkinsufi@gmail.com>
2024-05-16 21:44:33 +0400

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,62 @@
+"use client";
+
 import Image from "next/image";
+import Tilt from "react-parallax-tilt";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <main className="bg-[url('https://westgate.kiwi/wp-content/uploads/sites/8/2023/03/shutterstock_1029310603-scaled.jpg')] text-white h-svh flex items-center justify-center bg-no-repeat bg-cover">
+      <Tilt
+        className="background-stripes backdrop-blur-md parallax-effect"
+        perspective={2000} // Increase the perspective value for a less pronounced effect
+        scale={1} // Keep scale at 1 to avoid additional scaling
+        tiltMaxAngleX={10} // Reduce the maximum tilt angle to make it less sensitive
+        tiltMaxAngleY={10} // Reduce the maximum tilt angle to make it less sensitive
+        transitionSpeed={1500} // Smooth transition on mouse leave
+        tiltReverse={true}
+      >
+        <div className="bg-[#050505d9] backdrop-blur-md absolute left-0 top-0 rounded-[45px] rounded-r-none h-full w-[35%] flex items-center justify-center">
+          <div className="w-[90%] backdrop-blur-md h-[90%] flex items-center justify-center rounded-xl bg-[#3534345f]">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src={
+                "https://64.media.tumblr.com/1b5980c9f2f75f81cf059eee3e2d233c/tumblr_n79jfpE2O31tsa0m9o1_500.gif"
+              }
+              alt="fsdfsd"
+              width={300}
+              height={100}
             />
-          </a>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className="inner-element gap-[25px] grid grid-cols-3">
+          <div className="bg-[#ffffff18] rounded-[20px] aspect-w-1 aspect-h-1">
+            xidmət 
+          </div>
+          <div className="bg-[#ffffff18] rounded-[20px] aspect-w-1 aspect-h-1">
+            xidmət 
+          </div>
+          <div className="bg-[#ffffff18] rounded-[20px] aspect-w-1 aspect-h-1">
+            xidmət 
+          </div>
+          <div className="bg-[#ffffff18] rounded-[20px] aspect-w-1 aspect-h-1">
+            xidmət 
+          </div>
+          <div className="bg-[#ffffff18] rounded-[20px] aspect-w-1 aspect-h-1">
+            xidmət 
+          </div>
+          <div className="bg-[#ffffff18] rounded-[20px] aspect-w-1 aspect-h-1">
+            xidmət 
+          </div>
+          <div className="bg-[#ffffff18] rounded-[20px] aspect-w-1 aspect-h-1">
+            xidmət 
+          </div>
+          <div className="bg-[#ffffff18] rounded-[20px] aspect-w-1 aspect-h-1">
+            xidmət 
+          </div>
+          <div className="bg-[#ffffff18] rounded-[20px] aspect-w-1 aspect-h-1">
+            xidmət 
+          </div>
+        </div>
+      </Tilt>
     </main>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["64.media.tumblr.com"],
+  },
+};
 
 export default nextConfig;
